7f6004747fa0cef7f1035ca550aa50f7ac4e185e
joaocamargo <joao.camargo@gmail.com>
2022-08-20 11:54:45 -0300

    fim secao 4

---
+++ DatingApp/API/DTOs/LoginDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace API.DTOs
+{
+    public class LoginDto
+    {
+        [Required]
+        public string Username { get; set; }
+
+        [Required]
+        public string password { get; set; }
+
+    }
+
+
+}
+
+
---
+++ DatingApp/API/DTOs/RegisterDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace API.DTOs
+{
+    public class RegisterDto
+    {
+        [Required]
+        public string Username { get; set; }
+
+        [Required]
+        public string password { get; set; }
+
+    }
+
+
+}
+
+
