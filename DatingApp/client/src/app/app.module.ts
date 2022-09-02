e8a6fb8bd8d1f82f9eb9108bd8261d96995a28ec
joaocamargo <joao.camargo@gmail.com>
2022-09-02 00:24:14 -0300

    aula 80

--- DatingApp/client/src/app/app.module.ts
+++ DatingApp/client/src/app/app.module.ts
@@ -17,6 +17,7 @@ import { ListsComponent } from './lists/lists.component';
 import { MessagesComponent } from './messages/messages.component';
 import { ToastrModule } from 'ngx-toastr';
 import { SharedModule } from './_modules/shared.module';
+import { TestErrorsComponent } from './errors/test-errors/test-errors.component';
 
 
 @NgModule({
@@ -29,6 +30,7 @@ import { SharedModule } from './_modules/shared.module';
     MemberDetailComponent,
     ListsComponent,
     MessagesComponent,
+    TestErrorsComponent,
   ],
   imports: [
     BrowserModule,
