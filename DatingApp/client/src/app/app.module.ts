199b49a9265e6c3e5a368c4d9531dc0ce1d62322
joaocamargo <joao.camargo@gmail.com>
2022-09-08 17:56:59 -0300

    update angular

--- DatingApp/client/src/app/app.module.ts
+++ DatingApp/client/src/app/app.module.ts
@@ -18,7 +18,7 @@ import { MessagesComponent } from './messages/messages.component';
 import { ToastrModule } from 'ngx-toastr';
 import { SharedModule } from './_modules/shared.module';
 import { TestErrorsComponent } from './errors/test-errors/test-errors.component';
-
+import { ButtonModule } from 'primeng/button';
 
 @NgModule({
   declarations: [
@@ -39,7 +39,8 @@ import { TestErrorsComponent } from './errors/test-errors/test-errors.component'
     BrowserAnimationsModule,
     FormsModule,
     CommonModule,
-    SharedModule
+    SharedModule,
+    ButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
